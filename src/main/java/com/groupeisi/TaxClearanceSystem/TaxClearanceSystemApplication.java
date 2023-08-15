ddd9e5d07dab4423b5bf3a3ae1aa295a75e67af1
thiareMohamed <thiaremohamed.mt@gmail.com>
2023-08-15 10:51:40 +0000

    config datasource

---
+++ src/main/java/com/groupeisi/TaxClearanceSystem/TaxClearanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.TaxClearanceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxClearanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxClearanceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeisi/TaxClearanceSystem/TaxClearanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.TaxClearanceSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxClearanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
